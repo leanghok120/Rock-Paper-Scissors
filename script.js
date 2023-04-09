333d69d22ec2c39adbf95370f9781ba6b2dfcda0
Astrox <112290652+leanghok120@users.noreply.github.com>
2023-04-09 07:53:13 +0700

    Displays the what the user and computer picks

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+// Choices
+
 const computerChoices = ["paper", "scissors", "rock"];
+// H3 Elements
+
 const result = document.getElementById("result");
 const totalWinsElement = document.getElementById("totalWins");
+const userChoiceElement = document.getElementById("userChoice");
+const computerChoiceElement = document.getElementById("computerChoice");
+
 let totalWins = 0;
 
 const totalWinsStorage = localStorage.getItem("totalWins");
 if (totalWinsStorage !== null) {
   totalWins = parseInt(totalWinsStorage);
+  increaseWins(0);
 }
 
 // Makes the computer choose a random choice
@@ -21,11 +29,14 @@ function userPaper() {
   const computer = computerChoice();
   if (computer === "rock") {
     result.textContent = "You win!";
-    increaseWins();
+    increaseWins(1);
+    choice("paper", computer);
   } else if (computer === "paper") {
     result.textContent = "Tie!";
+    choice("paper", computer);
   } else {
     result.textContent = "You lose!";
+    choice("paper", computer);
   }
 }
 
@@ -35,12 +46,14 @@ function userRock() {
   const computer = computerChoice();
   if (computer === "scissors") {
     result.textContent = "You win!";
-    increaseWins();
-    totalWins.textContent = parseInt(totalWins.textContent) + 1;
+    increaseWins(1);
+    choice("rock", computer);
   } else if (computer === "rock") {
     result.textContent = "Tie!";
+    choice("rock", computer);
   } else {
     result.textContent = "You lose!";
+    choice("rock", computer);
   }
 }
 
@@ -50,16 +63,28 @@ function userScissors() {
   const computer = computerChoice();
   if (computer === "paper") {
     result.textContent = "You win!";
-    increaseWins();
+    increaseWins(1);
+    choice("scissors", computer);
   } else if (computer === "scissors") {
     result.textContent = "Tie!";
+    choice("scissors", computer);
   } else {
     result.textContent = "You lose!";
+    choice("scissors", computer);
   }
 }
 
-function increaseWins() {
-  totalWins += 1;
+// Increases the total wins
+
+function increaseWins(amount) {
+  totalWins += amount;
   totalWinsElement.textContent = `Total Wins: ${totalWins}`;
   localStorage.setItem("totalWins", totalWins);
+}
+
+// Displays the choices
+
+function choice(userChoices, computerChoices) {
+  userChoiceElement.textContent = `You: ${userChoices}`;
+  computerChoiceElement.textContent = `Computer: ${computerChoices}`;
 }
